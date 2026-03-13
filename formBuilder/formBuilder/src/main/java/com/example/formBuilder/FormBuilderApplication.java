74930a003c8a942828e8fae56ce55756dc405f69
krish patel office pc <krish.p@silvertouch.com>
2026-03-13 10:09:26 +0530

    fix some bug in user module

--- formBuilder/formBuilder/src/main/java/com/example/formBuilder/FormBuilderApplication.java
+++ formBuilder/formBuilder/src/main/java/com/example/formBuilder/FormBuilderApplication.java
@@ -1,7 +1,10 @@
 package com.example.formBuilder;
-
+// ... existing imports ...
+import com.example.formBuilder.service.ModuleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FormBuilderApplication {
@@ -10,4 +13,10 @@ public class FormBuilderApplication {
 		SpringApplication.run(FormBuilderApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(ModuleService moduleService) {
+		return args -> {
+			moduleService.seedModules();
+		};
+	}
 }
