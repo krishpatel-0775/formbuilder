e6e5205c2613b21ebf2ac66650700f3650bc2068
krishpatel-0775 <krishpatel0775@gmail.com>
2026-05-28 23:03:04 +0530

    add validation on sign up page
    add email and phone validation on sign up page

--- formBuilder/formBuilder/src/main/java/com/example/formBuilder/dto/RegisterRequest.java
+++ formBuilder/formBuilder/src/main/java/com/example/formBuilder/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.example.formBuilder.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -13,10 +14,20 @@ public class RegisterRequest {
     @NotBlank(message = "Full name is required")
     private String fullName;
 
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^[6-9]\\d{9}$",
+            message = "Invalid Indian phone number"
+    )
     private String phoneNumber;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Invalid email format"
+    )
     private String email;
 
     @NotBlank(message = "Password is required")
